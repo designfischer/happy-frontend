0112be0f722e9bcf31f70acac95bbb6ca2b76c35
Rafael Fischer <rafaelsanfischer@gmail.com>
2020-10-22 17:26:17 -0300

    happy-frontend

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+import styled from 'styled-components'
+
+const LandingPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+             
+        </>
+    )
+}
+
+export default LandingPage
+
+const BlueDiv = styled.div`
+    background-color: blue;
+`
